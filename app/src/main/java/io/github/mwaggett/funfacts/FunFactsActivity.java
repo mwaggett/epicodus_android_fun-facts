619af7b47854758644777c5cfde328676053b6d6
Molly Waggett <mwaggett@alumni.reed.edu>
2015-10-19 15:16:14 -0700

    Add ColorWheel class to randomly select a color and change the background to that color when button is clicked.

--- app/src/main/java/io/github/mwaggett/funfacts/FunFactsActivity.java
+++ app/src/main/java/io/github/mwaggett/funfacts/FunFactsActivity.java
@@ -1,11 +1,13 @@
 package io.github.mwaggett.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -13,6 +15,7 @@ import java.util.Random;
 public class FunFactsActivity extends AppCompatActivity {
 
     private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +24,18 @@ public class FunFactsActivity extends AppCompatActivity {
 
         // Declare our View variables and assign them the Views from the layout file
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
-        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String fact = mFactBook.getFact();
                 factLabel.setText(fact);
+
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
             }
         };
         showFactButton.setOnClickListener(listener);
