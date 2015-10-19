f9eba3d205dc8414fb14684b59d46d5e011fd711
Molly Waggett <mwaggett@alumni.reed.edu>
2015-10-19 14:40:54 -0700

    Store facts in an array and access them by choosing a random index.

--- app/src/main/java/io/github/mwaggett/funfacts/FunFactsActivity.java
+++ app/src/main/java/io/github/mwaggett/funfacts/FunFactsActivity.java
@@ -23,22 +23,25 @@ public class FunFactsActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built." };
+
                 // The button was clicked, so update the fact label with a new facts
                 String fact = "";
                 // Randomly select a fact
                 Random randomGenerator = new Random(); // Construct a new random number generator
-                int randomNumber = randomGenerator.nextInt(3);
+                int randomNumber = randomGenerator.nextInt(facts.length);
 
-                // Conver the random number to a text fact
-                if (randomNumber == 0) {
-                    fact = "Ants stretch when they wake up in the morning.";
-                } else if (randomNumber == 1) {
-                    fact = "Ostriches can run faster than horses.";
-                } else if (randomNumber == 2) {
-                    fact = "Olympic gold medals are actually made mostly of silver.";
-                } else {
-                    fact = "Sorry, there was an error!";
-                }
+                fact = facts[randomNumber];
 
                 // Update the label with our dynamic fact
                 factLabel.setText(fact);
