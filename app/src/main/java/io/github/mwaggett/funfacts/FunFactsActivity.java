5896019ba0c9738b0bba175b0a98c2ac53b09fcb
mwaggett <mwaggett@alumni.reed.edu>
2015-10-19 11:44:45 -0700

    Attach onClickListener that adds a new fact when button is clicked.

--- app/src/main/java/io/github/mwaggett/funfacts/FunFactsActivity.java
+++ app/src/main/java/io/github/mwaggett/funfacts/FunFactsActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
 
@@ -11,6 +14,19 @@ public class FunFactsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // Declare our View variables and assign them the Views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The button was clicked, so update the fact label with a new facts
+                String fact = "Ostriches can run faster than horses";
+                factLabel.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 
     @Override
